e8fec2c5828fbcc79dae2bf70b7a5f33945b7712
Nemcodespace <nemcodespace@gmail.com>
2025-06-13 20:43:51 -0300

    update LÓGICA PROGRAMAÇÃO/operadores-de-comparacao.js

--- LÓGICA PROGRAMAÇÃO/operadores-de-comparacao.js
+++ LÓGICA PROGRAMAÇÃO/operadores-de-comparacao.js
@@ -1,2 +1,21 @@
 //operadores de comparação 
+/*
+
+> maior que 
+>= maior ou igual a 
+< menor que 
+<= menor ou igual a 
+== igualdade ( checa valor)
+=== igualdade estrito (checa valor e tipo)
+!= diferente (checa valor)
+!== diferente estrito (checa valor e tipo)
+
+*/
+
+console.log(10 > 2)
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 != 10)
+console.log(10 !== '10')//true
+
 
