7ebe2407f468d3fd696cf83eff251af08a96d165
Nemcodespace <nemcodespace@gmail.com>
2025-06-13 23:05:31 -0300

    update LÓGICA PROGRAMAÇÃO/Operadores-logicos.js

---
+++ LÓGICA PROGRAMAÇÃO/Operadores-logicos.js
@@ -0,0 +1,20 @@
+//operadores logicos
+/*
+
+
+&&  AND 
+|| OR 
+! NEGAÇÃO 
+
+
+*/
+
+const n1 = 10
+const n2 = 20
+
+
+if ((n1 !== n2) || (n1 !== 20)) {
+  console.log("verdadeiro")
+}else {
+  console.log("false")
+}
